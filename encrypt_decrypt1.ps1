b27390f6c9877a1bf89a5fff990dd9fc43186315
BigChildsRep <andreas.rehfeldt@outlook.com>
2019-03-28 16:25:18 +0100

    special decrypt

---
+++ encrypt_decrypt1.ps1
@@ -0,0 +1,39 @@
+﻿function Decrypt-Text
+{
+  
+  param
+  (
+    [String]
+    [Parameter(Mandatory,ValueFromPipeline)]
+    $EncryptedText
+  )
+  process
+  {
+    $secureString = $EncryptedText | ConvertTo-SecureString
+    $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($secureString)
+    [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+  }
+}
+ 
+function Encrypt-Text
+{
+  
+  param
+  (
+    [String]
+    [Parameter(Mandatory,ValueFromPipeline)]
+    $Text
+  )
+  process
+  {
+     $Text |
+       ConvertTo-SecureString -AsPlainText -Force |
+       ConvertFrom-SecureString
+  }
+}
+$anton =read-host -AsSecureString
+$anton|ConvertFrom-SecureString|decrypt-text
+
+
+'PowerShell Rocks' | Encrypt-Text
+'Hello, World!' | Encrypt-Text | Decrypt-Text 
